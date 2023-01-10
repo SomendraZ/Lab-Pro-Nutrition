b6e6f815a2456efb816cc53d4006225cc9288ef7
SomendraZ <somendra.swaroop@kalvium.community>
2023-01-10 15:01:19 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import './App.css';
+// import Pronutrition from './components/pro-nutrition';
+import FoodData from './resources/FoodData';
+import Hooks from './components/hooks';
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Pronutrition test={FoodData}/> */}
+      <Hooks test={FoodData}/>
+    </div>
+  );
+}
+
+export default App;
